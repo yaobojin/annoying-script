f48aa3f35325c500582ba13674c53ab94c332b0c
yaobojin <yaojim@ymail.com>
2017-03-19 23:23:55 -0400

    Update ActualScriptYAY.c

--- ActualScriptYAY.c
+++ ActualScriptYAY.c
@@ -8,7 +8,7 @@ int main()
 FILE *f=fopen("AnnoyingScript","w");
 
 time_t now1;
-int hours;
+int hours, i=0;
 
 now1 = time(NULL);
 hours=now1/3600;
@@ -16,15 +16,20 @@ printf("Hours since January 1, 1970 = %ld\n", hours);
 
 for(;;)
 {
-    if ((hours%12)==0)
+    if (((hours%12)==0)&&(i==0))
     {
         fprintf(f,":)\n");
         printf("%d\n", hours);
         now1=time(NULL);
+        i=1;
+
     }
+    else if (((hours%12)==0)&&(i==1))
+        continue;
     else
     {
         now1=time(NULL);
+        i=0;
         continue;
     }
 }
